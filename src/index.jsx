fd6eb939f1bb9ef4befb45cf8e29e32ee4f67406
Harjas Singh <harjas.singh.dev@gmail.com>
2026-08-29 17:13:14 +0530

    refactor: migrate from Create React App to Vite, update dependencies, and restructure project files
    - Updated dependencies in package.json, including axios, react, and react-dom.
    - Removed public/index.html and src/App.js, replacing them with new index.html and App.jsx files.
    - Introduced index.jsx as the new entry point for the application.
    - Added vite.config.mjs for Vite configuration.
    - Adjusted scripts in package.json to use Vite commands for development and build processes.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "antd/dist/antd.css";
+import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
